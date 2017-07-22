5db9ad29d8771b4829277d6812ddd7a38428511c
skymeteor <skymeteor@gmail.com>
2017-07-23 07:18:29 +0800

    lianjia

---
+++ ajk/spiders/lianjia.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import datetime
+import time
+from ajk.items import AjkItem as ajk
+import random
+import scrapy.http.response
+import re
+import requests
+import json
+import re
+
+
+class LianjiaSpider(scrapy.Spider):
+    name = 'lianjia'
+    allowed_domains = ['lianjia.com']
+    start_urls = ['http://sh.lianjia.com/xiaoqu']
+
+    def parse(self, response):
+        district_name = response.xpath('//div[@class="option-list gio_district"]/a[@class!="on" and not(contains(text(),"周边"))]/text()').extract()
+        district_url = response.xpath('//div[@class="option-list gio_district"]/a[@class!="on" and not(contains(text(),"周边"))]/@href').extract()
+        for name, url in zip(district_name, district_url):
+            print("开始区=========")
+            print(url)
+            yield scrapy.Request(url=response.urljoin(url), callback=self.town)
+            time.sleep(random.randint(1,3))
+
+    def town(self,response):
+        town_names = response.xpath('//div[@class="option-list sub-option-list gio_plate"]/a[@class!="on"]/text()').extract() 
+        town_urls = response.xpath('//div[@class="option-list sub-option-list gio_plate"]/a[@class!="on"]/@href').extract() 
+        for name, url in zip(town_names, town_urls):
+            print("开始镇=========")
+            print(url)
+            yield scrapy.Request(url=response.urljoin(url),callback=self.town_data)
+            time.sleep(random.randint(1,3))
+
+    def town_data(self,response):
+        ershou = ajk()
+        data_area = response.xpath('//div[@class="list-wrap"]')
+        names = []
+        prices = []
+        bdyears = []
+        bdaddrs = []
+        lats = []
+        lngs = []
+        cdates=[]
+        for data in data_area:
+            name = data.xpath('//div[@class="info-panel"]/h2/a/text()').extract()         
+            price = data.xpath('//div[@class="info-panel"]/div[@class="col-3"]/div[@class="price"]/span[@class="num"]/text()').extract()
+            print(price)
+            if name:
+                names.append(name[0])
+                lats.append(' ')
+                lngs.append(' ')
+                bdyears.append(' ')
+                bdaddrs.append(' ')
+
+            if price:
+                tmp = re.findall("[-+]?\d+[\.]?\d*",price[0].strip().rstrip().lstrip())
+                prices.append(tmp[0])
+            elif not price:
+                prices.append('暂无均价')
+
+            dt=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            cdates.append(dt)
+            
+        for block_name, block_price,block_bdyear,block_bdaddr,block_lat,block_lng,block_date in zip(names, prices, bdyears,bdaddrs,lats,lngs,cdates):
+            ershou['house_name'] = block_name
+            ershou['house_price'] = block_price
+            ershou['house_bdyear'] = block_bdyear
+            ershou['house_bdaddr'] = block_bdaddr
+            ershou['house_lat'] = block_lat
+            ershou['house_lng'] = block_lng
+            ershou['craw_date'] = block_date
+            yield ershou
+            next_link = response.xpath('//div[@class="page-box house-list-page-box"]/a[contains(text(), "下一页")]/@href').extract()
+            print(next_link)
+            if next_link:
+                url = next_link[0]
+                print('next page ============='+url)
+                time.sleep(random.randint(1,3))
+                yield scrapy.Request(url=response.urljoin(url), callback=self.town_data)
